6a5189136b71fb8d3c05932dd711eb7b223d4ca6
Taylan Deniz Polat <taylandenizpolat@hotmail.com>
2023-08-28 23:50:00 +0300

    application layer added

--- Core.Persistance/Repositories/IAsyncRepository.cs
+++ Core.Persistance/Repositories/IAsyncRepository.cs
@@ -21,6 +21,17 @@ public interface IAsyncRepository<TEntity, TEntityId> : IQuery<TEntity>
             bool enableTracking = true,
             CancellationToken cancellationToken = default);
 
+    Task<Paginate<TEntity>> GetListAsync(
+      Expression<Func<TEntity, bool>>? predicate = null,
+      Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
+      Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+      int index = 0,
+      int size = 10,
+      bool withDeleted = false,
+      bool enableTracking = true,
+      CancellationToken cancellationToken = default
+    );
+
     Task<Paginate<TEntity>> GetListByDynamicAsync(
     DynamicQuery dynamic,
     Expression<Func<TEntity, bool>>? predicate = null,
